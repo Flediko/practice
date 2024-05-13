01cae6cd24406acaab8f26ece8eb7eccc8fe6aa8
Flediko <160780745+Flediko@users.noreply.github.com>
2024-05-13 23:20:38 +0530

    Update ques 13.c

--- ques 13.c
+++ ques 13.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+
+int isDigit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int isLetter(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
 
 int main() {
     FILE *p1, *p2;
     char line[1000];
-    int c,d;
+    int c, d;
     p1=fopen("File.txt", "r");
-    if(p1==NULL)
+    if (p1==NULL)
     {
         printf("Error opening the input file.\n");
         return 1;
     }
     p2=fopen("Count.txt", "w");
-    if(p1==NULL)
+    if (p2==NULL)
     {
         printf("Error creating the output file.\n");
         fclose(p1);
         return 1;
     }
-    while (fgets(line,sizeof(line),p1)!=NULL)
+    while (fgets(line, sizeof(line), p1) != NULL) 
     {
         c=d=0;
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (isalpha(line[i])) 
+        for (int i = 0; line[i] != '\0'; i++) 
+        {
+            if (isLetter(line[i])) 
             {
                 c++;
             } 
-            else if (isdigit(line[i])) 
+            else if (isDigit(line[i])) 
             {
                 d++;
             }
@@ -37,9 +45,6 @@ int main() {
     }
     fclose(p1);
     fclose(p2);
-
     printf("Character and digit counts written to Count.txt successfully.\n");
-
     return 0;
 }
-
